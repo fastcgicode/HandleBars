0d7dea07f401e0e150d65a63659b2ae21ff80fbb
Alexander Pourmirzayouf <fastcgicode@yahoo.com>
2024-02-17 13:38:20 -0800

    upd

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
     res.render('home');
 });
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+});
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+app.get('/logout', (req, res) => {
+    res.render('login');
+});
 app.listen(3000, () => {
     console.log('The web server has started on port 3000');
 });
